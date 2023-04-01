09b3e531287cd9dca19d2ecfa02d3df1563359e3
lee990429 <111167280+lee990429@users.noreply.github.com>
2023-04-01 22:32:17 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 9664 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -4,10 +4,10 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 let n = Number(input[0]);
 let score = input[1].split(' ').map(Number);
 
-let maxValue = score.reduce((a, b) => Math.max(a, b));
+let max = score.reduce((a, b) => Math.max(a, b));
 let answer = [];
 for(let i =0; i<n; i++){
-answer.push(score[i] / maxValue * 100);    
+answer.push(score[i] / max * 100);    
 }
 
 console.log(answer.reduce((a, b) => a + b)/ n);

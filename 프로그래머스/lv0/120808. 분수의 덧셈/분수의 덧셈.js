f62ea4687631a79120ee1700c1c52bf01f8edcc6
이수현 <111167280+lee990429@users.noreply.github.com>
2023-04-08 14:27:11 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,22 +1,22 @@
 function solution(numer1, denom1, numer2, denom2) {
    
+    //분자
+    let numer = numer1 * denom2 + numer2 * denom1;
     
-
-    let denom = numer1 * denom2 + numer2 * denom1;
+    //분모    
+    let denom = denom1 * denom2;
     
- 
-    let num = denom1 * denom2;
-
+    
     let max =1;
     
    
     for(let i =1; i<= denom; i++){
-        if(denom%i === 0 && num%i===0){
+        if(denom%i === 0 && numer%i===0){
             max = i
         }
     }
     
    
     
-    return [denom/max, num/max]
+    return [numer/max, denom/max]
 }

d346def431de8d786f2179488f113df9081e4ef6
이수현 <111167280+lee990429@users.noreply.github.com>
2023-04-09 19:56:40 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,16 +1,16 @@
-function solution(array) {
-    let counting = new Map();
-    let countArray = new Array;
-    let max = 0;
-    for(let i of array) {
-        if(!counting.has(i)) counting.set(i, 0);
-        if(counting.has(i)) counting.set(i, counting.get(i)+1);
-        while(counting.get(i) > max) max++;
-    }
 
-    for (let [k, v] of counting) {
-        if(v === max) countArray.push(k)
+function solution(array) {
+    let map = new Map();
+    let count = new Array;
+    let mod = 0;
+    for(let i of array){
+        if(!map.has(i)) map.set(i, 0);
+        if(map.has(i)) map.set(i, map.get(i) +1);
+        while(map.get(i) > mod) mod++;
+    }
+    for(let[k, v] of map){
+        if ( v === mod) count.push(k)
     }
     
-    return countArray.length === 1 ? countArray[0] : -1;
+    return count.length === 1 ? count[0] : -1;
 }

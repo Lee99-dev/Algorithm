4cc62a7d96c4b6f1856ed777d0a8b9d9e45d0585
이수현 <111167280+lee990429@users.noreply.github.com>
2023-04-11 17:46:08 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 9620 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -4,13 +4,14 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 let n = Number(input[0]);
 let p = input[1].split(' ').map(Number);
 
-p.sort((a,b) => a - b);
-let answer = 0;
-let time = 0;
-for(let i = 0; i < n; i++){
-//이 경우 1,2,3,4,5 차례로 돌아감
+p.sort((a, b) => a - b);
 
-    time +=p[i];
+
+let time = 0;
+let answer = 0;
+
+for(let i = 0; i < n; i++){
+    time += p[i];
     answer += time;
 }
 

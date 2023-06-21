f285082cb3adafe017b736364ecef753bc0b2680
이수현 <111167280+lee990429@users.noreply.github.com>
2023-06-22 00:03:00 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    
+    for(let i = 1; i <= n; i++){
+        if(i * i === n ) {
+           return 1;
+        }
+        if(i * i > n ) {
+            return 2;
+        }
+    }
+    
+    
+}

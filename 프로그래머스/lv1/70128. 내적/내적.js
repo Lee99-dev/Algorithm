47f4a365102fa44a9e1acc4fa447a0f67e6150e3
soohyeon.lee <111167280+lee990429@users.noreply.github.com>
2023-07-24 09:44:11 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,3 +1,8 @@
 function solution(a, b) {
-    return a.reduce((acc, _, i) => acc += a[i] * b[i], 0)
+    let answer = 0;
+    for(let i = 0; i < a.length; i++){
+        answer += a[i] * b[i];
+    }
+    
+    return answer;
 }

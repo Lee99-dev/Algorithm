347fa25ed397703c0799fb7509fe6949b91da847
soohyeon.lee <111167280+lee990429@users.noreply.github.com>
2023-08-16 16:30:39 +0900

    [Silver II] Title: 예산, Time: 192 ms, Memory: 11880 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.js
+++ 백준/Silver/2512. 예산/예산.js
@@ -5,23 +5,25 @@ let n = Number(input[0].split(' ')[0]);
 let arr = input[1].split(' ').map(Number);
 let m = Number(input[2]);
 
-let start = 1;
-let end = arr.reduce((a, b) => Math.max(a, b));
-let result = 0;
+let left = 1;
+let right = arr.reduce((a, b) => Math.max(a, b));
 
-while (start <= end) {
-let mid = parseInt((start + end) / 2);
+let answer = 0;
+
+while (left <= right) {
+let mid = parseInt((left + right) / 2);
 let total = 0;
 for (x of arr) {
 total += Math.min(mid, x);
 }
-    
-if (total <= m) {
-result = mid;
-start = mid + 1;
+
+if(total <= m) {
+answer = mid;
+left = mid + 1;
+}else {
+right = mid - 1;
 }
-else {
-end = mid - 1;
 }
-}
-console.log(result);
+
+console.log(answer);
+

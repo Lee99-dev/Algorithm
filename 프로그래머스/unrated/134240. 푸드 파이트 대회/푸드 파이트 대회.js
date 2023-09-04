4bc192897ed5d148f885496db7a1ddb941ea73f4
soohyeon.lee <111167280+lee990429@users.noreply.github.com>
2023-09-04 14:35:31 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,11 @@
+function solution(food) {
+    let answer = '';
+    
+    for(let i = 1; i<food.length; i++){
+        if(food[i] >=2){
+            answer += String(i).repeat(parseInt(food[i] /2));
+        }
+    }
+    
+    return answer + 0 + answer.split('').reverse().join('');
+}

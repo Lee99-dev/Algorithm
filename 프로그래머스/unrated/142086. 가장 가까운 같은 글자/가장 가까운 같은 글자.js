e22e33236679cdabad60797b87eacd6517e299a7
Lee_dev <111167280+Lee99-dev@users.noreply.github.com>
2023-09-20 22:46:12 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.58 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    let answer = [];
+    
+    const map = new Map();
+    [...s].forEach((c, i) => {
+        answer.push(map.get(c) > -1 ? i - map.get(c) : -1)
+        map.set(c, i)
+    })
+    
+    return answer;
+}
